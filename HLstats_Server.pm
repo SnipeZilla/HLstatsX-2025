44a933238a8672e5f173f5358bc99ab492881dc5
SnipeZilla <cedde@msn.com>
2025-08-01 08:00:54 -0400

    Update HLstats_Server.pm

--- HLstats_Server.pm
+++ HLstats_Server.pm
@@ -557,27 +557,28 @@ sub track_server_load_async {
             my $new_timestamp = time();
 
             my $string = $self->{play_game} == CS2()? $self->dorcon("status_json") : $self->dorcon("stats");
-
+             
             if ($string eq "") {
                 $self->set("track_server_timestamp", $new_timestamp);
                 $err = "[track_server_load] No RCON response from " . $self->{address};
                 return $queue->enqueue([$err, undef]);
             }
-
+            
+            my ($fps, $uptime, $map);
             if ( $self->{play_game} == CS2() ) {
 
-               my $fps = ($string =~ /"frametime_ms": ([\d.]+)/) ? sprintf("%.2f", 1000 / $1) : '0.00';
-               my ($uptime) = $string =~ /"process_uptime": ([\d]+)/;
-               my ($map)    = $string =~ /"map": "([^"]+)"/;
+               $fps      = ($string =~ /"frametime_ms": ([\d.]+)/) ? sprintf("%.2f", 1000 / $1) : '0.00';
+               ($uptime) = $string =~ /"process_uptime": ([\d]+)/;
+               ($map)    = $string =~ /"map": "([^"]+)"/;
 
             } else {
 
                 $string =~ /CPU.*\n(.*)\n*L{0,1}.*\Z/;
                 $string = $1;
                 $string =~ /([^ ]+)\s+([^ ]+)\s+([^ ]+)\s+([^ ]+)\s+([^ ]+)\s+([^ ]+)\s+([^ ]+)\s*([^ ]*)/;
-                my $uptime = $4;
-                my $fps = $6;
-                my $map = $self->{map};
+                $uptime = $4;
+                $fps = $6;
+                $map = $self->{map};
 
             }
 
